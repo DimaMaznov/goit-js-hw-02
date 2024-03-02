f99199edf829665af0ed1213ec29a847120bffe3
DimaMaznov <maznovdmitriy01@gmail.com>
2024-03-02 22:34:14 +0200

    1

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,19 @@
-"use strict";
 
-function getElementWidth (content, padding, border) 
+function checkForSpam(message) {
 
-   {  
-    return (
-        Number.parseFloat(content) +
-        Number.parseFloat(padding) * 2 +
-        Number.parseFloat(border) * 2
-      );}
+    const spam = 'spam';
+    const sale = 'sale';
+   const normalizeSpam = spam.toLowerCase();
+   const normalizeSale = sale.toLowerCase();
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+   return message.includes(normalizeSpam) || message.includes(normalizeSale);
+}
+
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
